f0a225d4accc0cebf33281d30b4418cccbe39d6f
aakashDeveloper <ahanda205@gmail.com>
2019-04-26 22:54:35 +0530

    A

--- firstapp/src/app/shared/star.component.ts
+++ firstapp/src/app/shared/star.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges,
+        OnInit, OnDestroy, Input } from '@angular/core';
 
 @Component({
     selector: 'app-star',
-    templateUrl: './star.component.html'
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
 })
 
-export class StarComponent {
+export class StarComponent implements OnChanges, OnInit, OnDestroy {
 
+    @Input() rating: number;
+    starWidth: number;
+
+    constructor() {
+        console.log('>>>>>constructor<<<<<<');
+    }
+
+    ngOnInit() {
+        console.log('>>>>>OnInit<<<<<<');
+    }
+
+    ngOnChanges() {
+        console.log('>>>>>OnChanges<<<<<<');
+        this.starWidth = this.rating * 86 / 6;
+    }
+
+    ngOnDestroy() {
+        console.log('>>>>>OnDestroy<<<<<<');
+    }
 }
